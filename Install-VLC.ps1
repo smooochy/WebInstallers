2909ee3449668737b079bc72c81a6715c7aace6f
smooochy <dannysomogyi@gmail.com>
2022-07-06 10:10:09 -0500

    Changed Invoke-WebRequest to Start-BitsTransfer

--- Install-VLC.ps1
+++ Install-VLC.ps1
@@ -1,7 +1,7 @@
 ﻿$URI = 'https://download.videolan.org/vlc/last/win64'
 $Installer = (Invoke-WebRequest -Uri $URI -UseBasicParsing).Links.href | Where-Object{$_ -like "*.msi"}
 
-Invoke-WebRequest -Uri "$URI\$Installer" -OutFile "C:\Temp\$Installer" -PassThru
+Start-BitsTransfer -Source "$URI/$Installer" -Destination "$Env:TEMP"
 
-Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i C:\Temp\$($Installer) /qb MSIFASTINSTALL=7" -Wait
-Remove-Item "C:\Temp\$($Installer)" -Force
+Start-Process -FilePath 'msiexec.exe' -ArgumentList "/i `"$Env:TEMP\$($Installer)`" /passive MSIFASTINSTALL=7" -Wait
+Remove-Item "$Env:TEMP\$($Installer)" -Force
